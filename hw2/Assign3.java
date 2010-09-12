b759f74a029d3e07a1797e80fda8aada825dfccc
Evan Travers <widgetwebworks@gmail.com>
2010-09-12 17:05:06 -0500

    I'm on my way

---
+++ hw2/Assign3.java
@@ -0,0 +1,20 @@
+// Assign3 class 
+// This class is a MicroScala parser which reads a MicroScala source 
+// program and outputs the set of syntax trees comprising that program's 
+// executable functions. 
+import java.io.*;
+
+public class Assign3 {
+
+  public static void main (String args []) throws java.io.IOException {
+
+    System . out . println ("Source Program");
+    System . out . println ("--------------");
+    System . out . println ();
+
+    Parser parser = new Parser ();
+    parser . CompilationUnit ();
+
+  }
+
+}
---
+++ hw2/Parser.java
@@ -0,0 +1,157 @@
+// Parser is a class to represent a recursive descent parser for the 
+// MicroScala programming language.	The parser also constructs a 
+// syntax tree representation of all executable constructions.
+
+import java.util.*;
+
+public class Parser {
+
+	protected MicroScalaLexer lexer;	// lexical analyzer
+	protected Token token;					 // current token
+
+	public Parser () throws java.io.IOException {
+		lexer = new MicroScalaLexer (System . in);
+		getToken ();
+	}
+
+	protected void getToken () throws java.io.IOException {
+		token = lexer . nextToken ();
+	}
+
+	// compilationUnit parses MicroScala programs.
+	public void CompilationUnit () throws java.io.IOException {
+		// object
+		if (token.symbol()!=TokenClass.OBJECT) {
+			ErrorMessage.print("object expected");
+		}
+		getToken();
+		
+		// id
+		if (token.symbol()!=TokenClass.ID) {
+			ErrorMessage.print("id expected");
+		}
+		getToken();
+		
+		// {
+		if (token.symbol()!=TokenClass.LEFTBRACE) {
+			ErrorMessage.print("Left Brace expected");
+		}
+		getToken();
+		
+		// one or more defs
+		while (token.symbol()==TokenClass.DEF||token.symbol()==TokenClass.VAR) {
+			Def();
+		}
+		getToken();
+		
+		// }
+		if (token.symbol()!=TokenClass.RIGHTBRACE) {
+			ErrorMessage.print("Right BRACE Expected");
+		}
+		getToken();
+				
+		if (token.symbol()!=TokenClass.EOF) {
+			ErrorMessage.print("EOF expected");			
+		}
+	}
+	
+	// Def
+	public void Def() throws java.io.IOException {
+		if (token.symbol()==TokenClass.VAR) {
+			// this is a var
+		}
+		
+		if (token.symbol()==TokenClass.DEF) {
+			getToken();
+			if (token.symbol()==TokenClass.MAIN) {
+				// this is a main
+			}
+			if (token.symbol()==TokenClass.ID) {
+				// normal def
+				getToken();
+				
+				if (token.symbol()!=TokenClass.LEFTPAREN) {
+					ErrorMessage.print("( expected");
+				}
+				getToken();
+				
+				if (token.symbol()!=TokenClass.ID) {
+					ErrorMessage.print("ID expected");
+				}
+				getToken();
+				
+				if (token.symbol()!=TokenClass.COLON) {
+					ErrorMessage.print(": expected");
+				}
+				getToken();
+				
+				Type();
+				
+				if (token.symbol()!=TokenClass.COMMA) {
+					ErrorMessage.print(", expected");
+				}
+				getToken();
+				
+				if (token.symbol()!=TokenClass.ID) {
+					ErrorMessage.print("ID expected");
+				}
+				getToken();
+
+				if (token.symbol()!=TokenClass.COLON) {
+					ErrorMessage.print(": expected");
+				}
+				getToken();
+
+				Type();
+				
+				if (token.symbol()!=TokenClass.RIGHTPAREN) {
+					ErrorMessage.print(") expected");
+				}
+				getToken();
+				
+				if (token.symbol()!=TokenClass.COLON) {
+					ErrorMessage.print(": expected");
+				}
+				getToken();
+				
+				
+			}
+		}
+	}
+	
+	public void Type() throws java.io.IOException {
+		if (token.symbol()==TokenClass.INT) {
+			getToken();
+		}
+		else if (token.symbol()==TokenClass.LIST) {
+			getToken();
+			if (token.symbol()!=TokenClass.LEFTBRACKET) {
+				ErrorMessage.print("[ expected");
+			}
+			getToken();
+			if (token.symbol()!=TokenClass.INT) {
+				ErrorMessage.print("int expected");
+			}
+			getToken();
+			if (token.symbol()!=TokenClass.RIGHTBRACKET) {
+				ErrorMessage.print("] expected");
+			}
+			getToken();
+		}
+		else {
+			ErrorMessage.print("Type expected");
+		}
+	}
+	
+	public void Literal() {
+		System.out.println("literal");
+	}
+	
+	public void Statement() {
+		System.out.println("statment");
+	}
+	
+	public void ListExpr() {
+		System.out.println("listexpr");
+	}
+}
